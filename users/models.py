d9d34751113c18e79c6e6237060e1662c9717467
Tmuat <Thomas_Muat@hotmail.com>
2021-02-26 17:53:23 +0000

    Added custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
