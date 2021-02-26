2e5689a7c98cd11354f8738de1bc5fb6f028adde
Tmuat <Thomas_Muat@hotmail.com>
2021-02-26 18:25:44 +0000

    Adjusted development settings

--- core/settings/development.py
+++ core/settings/development.py
@@ -4,18 +4,17 @@ from .base import *  # noqa
 
 from .base import env
 
-
+# ------------------------------------------------------------------------------
 # GENERAL
 # ------------------------------------------------------------------------------
-# # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-# SECRET_KEY = env.str("DJANGO_SECRET_KEY")
-# https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG", False)
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
+# ------------------------------------------------------------------------------
 # CACHES
 # ------------------------------------------------------------------------------
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
     "default": {
@@ -24,28 +23,36 @@ CACHES = {
     }
 }
 
+# ------------------------------------------------------------------------------
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
+
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+# INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+# MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
-DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
-    "SHOW_TEMPLATE_CONTEXT": True,
-}
+# DEBUG_TOOLBAR_CONFIG = {
+#     "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+#     "SHOW_TEMPLATE_CONTEXT": True,
+# }
+
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
-
+# ------------------------------------------------------------------------------
 # EMAILS
 # ------------------------------------------------------------------------------
+
 # https://docs.djangoproject.com/en/3.1/ref/settings/#email-backend
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# ------------------------------------------------------------------------------
 # DATABASES
 # ------------------------------------------------------------------------------
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
@@ -54,24 +61,33 @@ DATABASES = {
     }
 }
 
+# ------------------------------------------------------------------------------
 # STATIC
 # ------------------------------------------------------------------------------
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
+
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )  # noqa
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # noqa
 
+# ------------------------------------------------------------------------------
 # MEDIA
 # ------------------------------------------------------------------------------
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # noqa
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
 MEDIA_URL = '/media/'
 
+# ------------------------------------------------------------------------------
 # ENVIRONMENT NOTICE
 # ------------------------------------------------------------------------------
+
 # https://pypi.org/project/django-admin-env-notice/
 ENVIRONMENT_NAME = 'Development server'
 ENVIRONMENT_COLOR = '#FF2222'
