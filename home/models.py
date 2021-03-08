946bb5202eabf39f8afc928de85c6afc995e1f35
Tmuat <Thomas_Muat@hotmail.com>
2021-03-08 14:48:00 +0000

    Added testimonial model

--- home/models.py
+++ home/models.py
@@ -18,18 +18,31 @@ class UpdatedAndCreated(models.Model):
         abstract = True
 
 
-class Image(UpdatedAndCreated):
+class LandingImage(UpdatedAndCreated):
     image_location = models.CharField(max_length=50,
                                       null=False,
                                       blank=False)
-    image = models.ImageField(upload_to="images/",
+    image = models.ImageField(upload_to="images/landing-images",
                               null=True,
                               blank=False)
     active = models.BooleanField(default=False)
 
     class Meta:
-        verbose_name_plural = "Images"
+        verbose_name_plural = "Landing Images"
         ordering = ['created_at']
 
     def __str__(self):
         return self.image_location
+
+
+class Testimonial(UpdatedAndCreated):
+    name = models.CharField(max_length=255,
+                            null=False,
+                            blank=False)
+    testimonial = models.CharField(max_length=500,
+                                   null=False,
+                                   blank=False)
+    image = models.ImageField(upload_to="images/testimonial",
+                              null=True,
+                              blank=False)
+    active = models.BooleanField(default=False)
