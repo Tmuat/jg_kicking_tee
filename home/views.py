c815bc0fcb2a8140edd725ee90e599073e1481ab
Tmuat <Thomas_Muat@hotmail.com>
2021-03-08 13:48:56 +0000

    Added image QS to home screen

--- home/views.py
+++ home/views.py
@@ -1,11 +1,25 @@
+from random import shuffle
+
+
 from django.shortcuts import render
+
+
+from .models import Image
 
 
 def index(request):
     """
-    A view to return the index page.
+    A view to return the index page. Including admin uploaded images for
+    the gallery.
     """
+
+    images_qs = list(Image.objects.filter(active=True))
+    shuffle(images_qs)
+
+    context = {
+        'images': images_qs,
+    }
 
     template = 'home/index.html'
 
-    return render(request, template)
+    return render(request, template, context)
