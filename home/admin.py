f88a0ff8774b027151eccf2a8763931cbf488617
Tmuat <Thomas_Muat@hotmail.com>
2021-03-08 13:43:00 +0000

    Created image model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Image
+
+
+class CustomImageAdmin(admin.ModelAdmin):
+    list_display = ('image_location',
+                    'active',
+                    'image',
+                    'created_by',
+                    'created_at'
+                    )
+    list_filter = ('image_location', 'active',)
+    search_fields = ('name',)
+    ordering = ('image_location',)
+    readonly_fields = ['created_by', 'created_at', 'updated_by', 'updated_at']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user.username
+        obj.updated_by = request.user.username
+        obj.save()
+
+
+admin.site.register(Image, CustomImageAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,35 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class UpdatedAndCreated(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.CharField(max_length=100,
+                                  null=True,
+                                  blank=True)
+    updated_by = models.CharField(max_length=100,
+                                  null=True,
+                                  blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(UpdatedAndCreated):
+    image_location = models.CharField(max_length=50,
+                                      null=False,
+                                      blank=False)
+    image = models.ImageField(upload_to="images/",
+                              null=True,
+                              blank=False)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Images"
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.image_location
