542749ed97acbdd14160ffb5dbc1c6b791c34317
Tmuat <Thomas_Muat@hotmail.com>
2021-03-09 14:41:55 +0000

    Added contact form functionality

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,9 @@
 from honeypot.decorators import check_honeypot
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError, EmailMessage
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 from .forms import (
     ContactForm
@@ -14,6 +17,44 @@ def contact(request):
     to stop bots submitting the form.
     """
     form = ContactForm()
+    if request.POST:
+        form = ContactForm(request.POST or None)
+        if form.is_valid():
+            name = form.cleaned_data['from_name']
+            email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            subject = "Website Inquiry"
+
+            msg_contact = render_to_string('emails/contact.txt',
+                                           {'name': name,
+                                            'subject': subject,
+                                            'email': email,
+                                            'message': message})
+
+            msg_contact_confirm = render_to_string('emails/contact-confirm.txt',
+                                                   {'name': name,
+                                                    'subject': subject,
+                                                    'email': email,
+                                                    'message': message})
+            try:
+                email = EmailMessage(
+                    subject,
+                    msg_contact,
+                    '"JG Tee Contact" <info@jimmygtee.co.uk>',
+                    ['"JG Tee Contact" <info@jimmygtee.co.uk>'],
+                    reply_to=[email],
+                )
+                email.send(fail_silently=False)
+                send_mail(
+                    "Contact Confirmation",
+                    msg_contact_confirm,
+                    '"JG Tee Contact" <info@jimmygtee.co.uk>',
+                    [email],
+                    fail_silently=False
+                )
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('home')
 
     context = {
         "form": form,
