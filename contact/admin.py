33c84e9eddc955432888858cf70168610b1655fe
Tmuat <Thomas_Muat@hotmail.com>
2021-03-09 15:06:40 +0000

    Created model for user email

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EmailInfo
+
+
+class CustomEmailInfoAdmin(admin.ModelAdmin):
+    list_display = ('email_show_name',
+                    'email',
+                    'created_by',
+                    'created_at',
+                    'updated_by',
+                    'updated_at'
+                    )
+    list_filter = ('email',)
+    search_fields = ('email',)
+    readonly_fields = ['created_by', 'created_at', 'updated_by', 'updated_at']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user.email
+        obj.updated_by = request.user.email
+        obj.save()
+
+
+admin.site.register(EmailInfo, CustomEmailInfoAdmin)
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from home.models import UpdatedAndCreated
+
+
+class EmailInfo(UpdatedAndCreated):
+    email_show_name = models.CharField(max_length=80,
+                                       null=False,
+                                       blank=False)
+    email = models.EmailField()
+
+    class Meta:
+        verbose_name_plural = "Email Info's"
+
+    def __str__(self):
+        return self.email
