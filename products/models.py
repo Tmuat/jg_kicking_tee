d81c0f0442da74ae9e8d5669cbf2c3c6a72360ce
Tmuat <Thomas_Muat@hotmail.com>
2021-03-10 10:58:01 +0000

    Added new product images model

--- products/models.py
+++ products/models.py
@@ -12,8 +12,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -25,3 +23,12 @@ def pre_save_create_sku(sender, instance, *args, **kwargs):
 
 
 pre_save.connect(pre_save_create_sku, sender=Product)
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='product_images',
+    )
