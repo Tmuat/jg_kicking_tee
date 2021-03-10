a5cd14323ffc83f3742217e683d2c671a5a8395e
Tmuat <Thomas_Muat@hotmail.com>
2021-03-10 12:53:22 +0000

    Added product images qs

--- products/views.py
+++ products/views.py
@@ -7,9 +7,11 @@ def product_detail(request, product_slug):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, slug=product_slug)
+    product_images = ProductImage.objects.filter(product=product)
 
     context = {
         'product': product,
+        'product_images': product_images
     }
 
     return render(request, 'products/product_detail.html', context)
