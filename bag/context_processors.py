31b1dbf2c11c0e49dc4876005cc6bce190bea0dc
Tmuat <Thomas_Muat@hotmail.com>
2021-03-10 15:46:37 +0000

    Added extra context to bag

--- bag/context_processors.py
+++ bag/context_processors.py
@@ -1,3 +1,6 @@
+from django.shortcuts import get_object_or_404
+
+from products.models import Product
 
 
 def bag_contents(request):
@@ -5,6 +8,16 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for product_sku, quantity in bag.items():
+        product = get_object_or_404(Product, sku=product_sku)
+        total += quantity * product.price
+        bag_items.append({
+            'product_sku': product_sku,
+            'quantity': quantity,
+            'product': product
+        })
 
     context = {
         'bag_items': bag_items,
