8073019307b47540df54dec455f16224c72fd8bb
Tmuat <Thomas_Muat@hotmail.com>
2021-03-10 15:18:59 +0000

    Ajusted margins & added product featurs es to product detail

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Product, ProductImage
+from .models import Product, ProductImage, ProductFeature
 
 
 def product_detail(request, product_slug):
@@ -8,10 +8,12 @@ def product_detail(request, product_slug):
 
     product = get_object_or_404(Product, slug=product_slug)
     product_images = ProductImage.objects.filter(product=product)
+    product_features = ProductFeature.objects.filter(product=product)
 
     context = {
         'product': product,
-        'product_images': product_images
+        'product_images': product_images,
+        'product_features': product_features
     }
 
     return render(request, 'products/product_detail.html', context)
