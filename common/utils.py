f0613bba8dc07f62156fede55e9137386f16252f
Tmuat <Thomas_Muat@hotmail.com>
2021-03-11 16:09:09 +0000

    Created random order number generator

--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,8 @@
 import random
 import string
 
+from datetime import datetime
+
 
 def random_string_generator(size=5,
                             chars=string.digits):
@@ -17,3 +19,40 @@ def unique_sku_generator(instance):
     if qs_exists:
         return unique_sku_generator(instance)
     return new_sku
+
+
+def random_string_generator_orders(size=6,
+                                   chars=string.digits):
+    alphabet = {'1': 'a',
+                '2': 'b',
+                '3': 'c',
+                '4': 'd',
+                '5': 'e',
+                '6': 'f',
+                '7': 'g',
+                '8': 'h',
+                '9': 'i',
+                '10': 'j',
+                '11': 'k',
+                '12': 'l'}
+    current_month = str(datetime.now().month)
+
+    letter = alphabet[current_month].upper()
+
+    order = str(
+        letter + '-' + ''.join(random.choice(chars) for _ in range(size))
+    )
+
+    return order
+
+
+def unique_order_generator(instance):
+    new_order = random_string_generator_orders()
+
+    Klass = instance.__class__
+
+    qs_exists = Klass.objects.filter(
+                    order_number=new_order).exists()
+    if qs_exists:
+        return unique_order_generator(instance)
+    return new_order
