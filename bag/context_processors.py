84e84364a04092948bf7011aa4e7c53d67aecb21
Tmuat <Thomas_Muat@hotmail.com>
2021-03-12 18:39:06 +0000

    Adjusted context processor to account for delivery selection

--- bag/context_processors.py
+++ bag/context_processors.py
@@ -1,14 +1,23 @@
+from decimal import Decimal
+
 from django.shortcuts import get_object_or_404
 
 from products.models import Product
+from checkout.models import DeliveryOptions
 
 
 def bag_contents(request):
 
     bag_items = []
+    delivery_option = []
+
     total = 0
+    delivery_cost = 0
+    grand_total = 0
     product_count = 0
+
     bag = request.session.get('bag', {})
+    delivery_array = request.session.get('delivery', {})
 
     for product_sku, quantity in bag.items():
         product = get_object_or_404(Product, sku=product_sku)
@@ -18,12 +27,30 @@ def bag_contents(request):
             'product_sku': product_sku,
             'quantity': quantity,
             'product': product
+            })
+
+    for delivery_sku, price in delivery_array.items():
+        delivery = get_object_or_404(
+            DeliveryOptions, delivery_sku=delivery_sku)
+        delivery_cost = Decimal(price)
+        delivery_option.append({
+            'delivery': delivery
         })
+
+    if delivery_cost > 0:
+        delivery_set = True
+    else:
+        delivery_set = False
+
+    grand_total = total + delivery_cost
 
     context = {
         'bag_items': bag_items,
+        'product_count': product_count,
+        'delivery_option': delivery_option,
+        'delivery_set': delivery_set,
         'total': total,
-        'product_count': product_count
+        'grand_total': grand_total
     }
 
     return context
