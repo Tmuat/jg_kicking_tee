bd0adb95c6b429e4a9b817ad8dfbecfc86b78e4d
Tmuat <Thomas_Muat@hotmail.com>
2021-03-15 14:36:51 +0000

    Added more functionality to checkout & checkout success

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import checkout
+from .views import checkout, checkout_success
 
 
 urlpatterns = [
-    path('checkout/', checkout, name='checkout')
+    path('', checkout, name='checkout'),
+    path('checkout_success/<order_number>/',
+         checkout_success,
+         name='checkout_success')
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,14 @@
 import stripe
 
+from decimal import Decimal
+
 from django.conf import settings
 from django.contrib import messages
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 from .forms import OrderForm
+from .models import DeliveryOptions, OrderLineItem, Order
+from products.models import Product
 from bag.context_processors import bag_contents
 
 
@@ -12,26 +16,86 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    order_form = OrderForm()
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+        delivery_option = request.session.get('delivery', {})
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There is nothing in your bag at the moment")
-        return redirect(reverse('home'))
+        delivery_sku = delivery_option.get('option')
+        delivery = get_object_or_404(
+                DeliveryOptions, delivery_sku=delivery_sku)
+        delivery_cost = delivery.price
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            order.delivery_cost = delivery_cost
+            order.delivery_method = delivery
+            order.save()
+            for product_sku, quantity in bag.items():
+                try:
+                    product = Product.objects.get(sku=product_sku)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=quantity,
+                        )
+                    order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't "
+                        "found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('bag'))
+
+            # Save the info to the user's profile if all is well
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success',
+                                    args=[order.order_number]))
+        else:
+            messages.error(request, ('There was an error with your form. '
+                                     'Please double check your information.'))
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request,
+                           "There is nothing in your bag at the moment")
+            return redirect(reverse('bag'))
+
+        delivery_option = request.session.get('delivery', {})
+        if not delivery_option:
+            messages.error(request,
+                           "There is no delivery option selected")
+            return redirect(reverse('bag'))
+
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY
+        )
 
     if not stripe_public_key:
         messages.warning(request, ('Stripe public key is missing. '
                                    'Did you forget to set it in '
                                    'your environment?'))
+
+    order_form = OrderForm()
 
     template = 'checkout/checkout.html'
 
@@ -43,3 +107,27 @@ def checkout(request):
 
     return render(request, template, context)
 
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts.
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    if 'delivery' in request.session:
+        del request.session['delivery']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
