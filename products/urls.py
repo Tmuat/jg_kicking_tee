4e8d5633e13a246e8bf4f372e238a4d94f76c075
Tmuat <Thomas_Muat@hotmail.com>
2021-03-17 13:23:39 +0000

    Created url to edit product

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import product_detail
+from .views import product_detail, edit_product
 
 
 urlpatterns = [
-    path('product/<product_slug>', product_detail, name='product_detail')
+    path('product/<product_slug>', product_detail, name='product_detail'),
+    path('edit/<product_slug>/', edit_product, name='edit_product'),
 ]
