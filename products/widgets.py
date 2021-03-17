1b7d44dcfd243a76d5c41e4df7ed6ebe4f92d3d9
Tmuat <Thomas_Muat@hotmail.com>
2021-03-17 19:03:11 +0000

    Created custom image upload widget - widgets.py

--- products/widgets.py
+++ products/widgets.py
@@ -4,7 +4,6 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomClearableFileInput(widgets.ClearableFileInput):
     clear_checkbox_label = _('Remove')
-    initial_text = _('Current Images')
     input_text = _('')
     template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
 
