fb9833d71d80e7930f71dc08f37dd52cfb3e203d
Tmuat <Thomas_Muat@hotmail.com>
2021-03-18 16:14:07 +0000

    Added recent orders to admin home

--- site_admin/views.py
+++ site_admin/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
 
+from checkout.models import Order
+
+
 def admin_home(request):
     """
     A view to return the admin home page.
     """
 
+    orders = Order.objects.all().order_by('-date')[:8]
+
     context = {
+        'orders': orders,
     }
 
     template = 'site_admin/admin_home.html'
