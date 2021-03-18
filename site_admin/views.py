4f4126fba7e7a89c394710108575e7d5ba6f715d
Tmuat <Thomas_Muat@hotmail.com>
2021-03-18 17:04:54 +0000

    Created edit products in site_admin

--- site_admin/views.py
+++ site_admin/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 from checkout.models import Order
 from products.models import Product
-from products.forms import (
+from .forms import (
     ProductForm,
     ProductFeatureFormset,
     ProductImageFormset
---
+++ site_admin/widgets.py
@@ -0,0 +1,8 @@
+from django.forms import widgets
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomClearableFileInput(widgets.ClearableFileInput):
+    clear_checkbox_label = _('Remove')
+    input_text = _('')
+    template_name = 'site_admin/custom_widget_templates/custom_clearable_file_input.html'
