491acc00a693fb50b1a01c958fb63bea0c9ebd9b
Tmuat <Thomas_Muat@hotmail.com>
2021-03-18 14:31:58 +0000

    Added logging to bag

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.shortcuts import (
     render,
     redirect,
@@ -10,6 +12,9 @@ from django.contrib import messages
 from products.models import Product
 from checkout.models import DeliveryOptions
 
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
 
 def view_bag(request):
     """
@@ -17,6 +22,7 @@ def view_bag(request):
     """
 
     delivery = DeliveryOptions.objects.filter(active=True)
+    logger.info('Got Delivery Options')
 
     context = {
         'delivery': delivery,
@@ -32,10 +38,14 @@ def add_to_bag(request, product_id):
     Add a quantity of specified product to the shopping bag.
     """
     product = get_object_or_404(Product, id=product_id)
+    logger.debug(f'ATB: Found {product.name}')
 
     quantity = int(request.POST.get('quantity'))
+    logger.debug(f'ATB:Previous Quantity: {quantity}')
+
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
+    logger.debug(f'ATB: Previous Bag: {bag}')
 
     if product.sku in list(bag.keys()):
         bag[product.sku] += quantity
@@ -48,6 +58,8 @@ def add_to_bag(request, product_id):
 
     request.session['bag'] = bag
 
+    logger.debug(f'ATB: New Bag: {bag}')
+
     return redirect(redirect_url)
 
 
@@ -56,9 +68,12 @@ def adjust_bag(request, product_id):
     Adjust the quantity of specified product to the shopping bag.
     """
     product = get_object_or_404(Product, id=product_id)
+    logger.debug(f'AB: Found {product.name}')
 
     quantity = int(request.POST.get('quantity'))
+
     bag = request.session.get('bag', {})
+    logger.debug(f'AB: Previous Bag: {bag}')
 
     if quantity > 0:
         bag[product.sku] = quantity
@@ -72,6 +87,7 @@ def adjust_bag(request, product_id):
                           f'from your bag'))
 
     request.session['bag'] = bag
+    logger.debug(f'AB: New Bag: {bag}')
 
     return redirect(reverse('bag'))
 
@@ -83,16 +99,23 @@ def remove_from_bag(request, product_id):
 
     try:
         product = get_object_or_404(Product, id=product_id)
+        logger.debug(f'RFB: Found {product.name}')
+
         bag = request.session.get('bag', {})
+        logger.debug(f'RFB: Previous Bag: {bag}')
 
         bag.pop(product.sku)
+        logger.debug('RFB: Product Removed From Bag')
+
         messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
+        logger.debug(f'RFB: New Bag: {bag}')
         return HttpResponse(status=200)
 
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
+        logger.debug(f'RFB: Error removing item: {e}')
         return HttpResponse(status=500)
 
 
@@ -104,12 +127,15 @@ def add_delivery(request):
         selected = request.POST.get('id-selected')
 
         delivery = get_object_or_404(DeliveryOptions, delivery_sku=selected)
+        logger.debug(f'AD: Delivery Selected: {delivery}')
 
         delivery_option = request.session.get('delivery', {})
 
         delivery_option.clear()
+        logger.debug('AD: Old Delivery In Session Deleted!')
 
         delivery_option['option'] = delivery.delivery_sku
+        logger.debug(f'AD: New Delivery Option Set {delivery.delivery_sku}')
 
         messages.success(request,
                          f'{delivery.option} - £{delivery.price} '
