df426f0a9de5f6bd4283980252463328c48f5950
Tmuat <Thomas_Muat@hotmail.com>
2021-03-18 16:48:06 +0000

    Created basic edit product in admin section

--- site_admin/urls.py
+++ site_admin/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import admin_home
+from .views import admin_home, admin_edit_product
 
 urlpatterns = [
-    path('home/', admin_home, name='admin_home')
+    path('home/', admin_home, name='admin_home'),
+    path('edit/<product_slug>/',
+         admin_edit_product,
+         name='admin_edit_product'),
 ]
--- site_admin/views.py
+++ site_admin/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 
 from checkout.models import Order
+from products.models import Product
+from products.forms import (
+    ProductForm,
+    ProductFeatureFormset,
+    ProductImageFormset
+)
 
 
 def admin_home(request):
@@ -16,5 +23,54 @@ def admin_home(request):
     }
 
     template = 'site_admin/admin_home.html'
+
+    return render(request, template, context)
+
+
+def admin_edit_product(request, product_slug):
+    """
+    A view to Edit a product in the store.
+    """
+
+    if not request.user.is_staff:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, slug=product_slug)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        formset = ProductFeatureFormset(
+            request.POST,
+            request.FILES,
+            instance=product
+        )
+        image_formset = ProductImageFormset(
+            request.POST,
+            request.FILES,
+            instance=product
+        )
+        if form.is_valid() and formset.is_valid() and image_formset.is_valid():
+            form.save()
+            formset.save()
+            image_formset.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.slug]))
+        else:
+            messages.error(request,
+                           ('Failed to update product. '
+                            'Please ensure the form is valid.'))
+    else:
+        form = ProductForm(instance=product)
+        formset = ProductFeatureFormset(instance=product)
+        image_formset = ProductImageFormset(instance=product)
+
+    template = 'site_admin/admin_product_management.html'
+    context = {
+        'form': form,
+        'formset': formset,
+        'image_formset': image_formset,
+        'product': product,
+    }
 
     return render(request, template, context)
