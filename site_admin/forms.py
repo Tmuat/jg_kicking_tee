e2f6afdb537a7f427e470fcf6d2658ea0ef10c76
Tmuat <Thomas_Muat@hotmail.com>
2021-03-22 10:56:56 +0000

    Styled delivery for smaller screens

--- site_admin/forms.py
+++ site_admin/forms.py
@@ -74,12 +74,17 @@ class DeliveryForm(forms.ModelForm):
         model = DeliveryOptions
         fields = 'option', 'description', 'price', 'active'
 
-    # description = forms.CharField(widget=forms.Textarea)
+    option = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 3, }))
+
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 3, }))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-0 rounded-0 w-100 h-100 no-active'
+            field.widget.attrs[
+                'class'] = 'border-0 rounded-0 w-100 h-100 no-active'
 
 
 DeliveryFormset = forms.modelformset_factory(
