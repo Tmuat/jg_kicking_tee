b3455b1d619a4648e468ee977b409aaa30469fe7
Tmuat <Thomas_Muat@hotmail.com>
2021-03-24 14:27:18 +0000

    Created custom file widget

--- site_admin/widgets.py
+++ site_admin/widgets.py
@@ -6,3 +6,9 @@ class CustomClearableFileInput(widgets.ClearableFileInput):
     clear_checkbox_label = _('Remove')
     input_text = _('')
     template_name = 'site_admin/custom_widget_templates/custom_clearable_file_input.html'
+
+
+class CustomClearableFileInputImages(widgets.ClearableFileInput):
+    clear_checkbox_label = _('Change')
+    input_text = _('')
+    template_name = 'site_admin/custom_widget_templates/custom_clearable_file_input_images.html'
