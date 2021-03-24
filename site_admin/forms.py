c320731730d5d19abcb88dfda83e52d271821620
Tmuat <Thomas_Muat@hotmail.com>
2021-03-24 14:26:40 +0000

    Created testimonial form

--- site_admin/forms.py
+++ site_admin/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 
-from products.models import Product, ProductFeature, ProductImage
+from products.models import (
+    Product,
+    ProductFeature,
+    ProductImage
+)
 from checkout.models import DeliveryOptions
-from .widgets import CustomClearableFileInput
+from home.models import (
+    LandingImage,
+    Testimonial
+)
+from .widgets import (
+    CustomClearableFileInput,
+    CustomClearableFileInputImages
+)
 
 
 class ProductForm(forms.ModelForm):
@@ -90,4 +101,34 @@ class DeliveryForm(forms.ModelForm):
 DeliveryFormset = forms.modelformset_factory(
         DeliveryOptions,
         form=DeliveryForm,
+    )
+
+
+class TestimonialForm(forms.ModelForm):
+
+    class Meta:
+        model = Testimonial
+        fields = 'name', 'testimonial', 'image', 'active'
+
+    testimonial = forms.CharField(
+        max_length=500,
+        widget=forms.Textarea(
+            attrs={'rows': 6, }
+        )
+    )
+
+    image = forms.ImageField(label='Image',
+                             required=False,
+                             widget=CustomClearableFileInputImages)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs[
+                'class'] = 'border-0 rounded-0 w-100 h-100 no-active new-image'
+
+
+TestimonialFormset = forms.modelformset_factory(
+        Testimonial,
+        form=TestimonialForm,
     )
